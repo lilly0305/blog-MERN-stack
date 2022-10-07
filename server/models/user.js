4d2319c141f8b42432a093f73e21584a146d9500
ryuhk <yhklilly0305@gmail.com>
2022-10-07 18:02:46 +0900

    userSchema done

---
+++ server/models/user.js
@@ -0,0 +1,48 @@
+import moment from "moment/moment";
+import mongoose from "mongoose";
+// TODO: Create Schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    requiered: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["MainJuin", "SubJuin", "User"],
+    default: "User",
+  },
+  register_date: {
+    type: Date,
+    default: moment().format("YYYY-MM-DD hh:mm:ss"),
+  },
+  comments: [
+    {
+      post_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+      },
+      comment_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comments",
+      },
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "post",
+    },
+  ],
+});
+
+const User = mongoose.model("user", UserSchema);
+export default User;
