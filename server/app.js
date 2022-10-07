8f675c7562446bbd676ba59945e5e27adec702ab
lilly0305 <yhklilly0305@gmail.com>
2022-10-07 23:40:29 +0900

    commit after connecting mongoDB done

--- server/app.js
+++ server/app.js
@@ -1,7 +1,30 @@
 import express from "express";
+import helmet from "helmet";
+import hpp from "hpp";
+import mongoose from "mongoose";
+import morgan from "morgan";
+import config from "./config";
+import cors from "cors";
 
 const app = express();
+const { MONGO_URI } = config;
 
+app.use(hpp());
+app.use(helmet());
+
+app.use(cors({ origin: true, Credential: true }));
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("MongoDB connecting Success!!"))
+  .catch((e) => console.log(e));
+
+// Use routes
 app.get("/");
 
 export default app;
