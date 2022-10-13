39e053ffe35c81c319cbcdc079f940915723722a
ryuhk <yhklilly0305@gmail.com>
2022-10-13 11:30:03 +0900

    Commit after redux setting done

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 
+// connectedRouter를 사용하여 리듀서 관련 함수를 불러올 때 'router'라 명명한 함수를 불러온다.
 const createRootReducer = (history) =>
   combineReducers({
     router: connectRouter(history),
---
+++ client/src/store.js
@@ -0,0 +1,33 @@
+import {
+  legacy_createStore as createStore,
+  compose,
+  applyMiddleWare,
+} from "redux";
+import createSagaMiddleware from "redux-saga";
+import { createBrowserHistory } from "history";
+import { routerMiddleware } from "connected-react-router";
+
+import createRootReducer from "./redux/reducers/index";
+import rootSaga from "./redux/sagas";
+
+export const history = createBrowserHistory();
+
+const sagaMiddleware = createSagaMiddleware();
+
+const initalState = {};
+const middlewares = [sagaMiddleware, routerMiddleware(history)];
+const devtools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+// 개발중일 때만 devtools 보이게 처리
+const composeEnhancer =
+  process.env.NODE_ENV === "production" ? compose : devtools || compose;
+
+const store = createStore(
+  createRootReducer(history),
+  initalState,
+  composeEnhancer(applyMiddleWare(...middlewares))
+);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
