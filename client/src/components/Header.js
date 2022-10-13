bb15da3b4a1cf29f78be87a0ce55debd27003c00
ryuhk <yhklilly0305@gmail.com>
2022-10-13 17:41:42 +0900

    Commit after header done

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,16 @@
+import { Col, Row } from "reactstrap";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div id="page-header" className="mb-3">
+      <Row>
+        <Col md="6" sm="auto" className="text-center m-auto">
+          <h1>Read Our Blog</h1>
+          <p>Lilly's project blog!🥰</p>
+        </Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Header;
